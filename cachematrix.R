051afa8e02aa7c0c6b7bfaeabfcec123ffbf9edd
litmd <litmd@gmail.com>
2014-06-22 15:17:31 -0600

    Solved some issues with non-square, incomplete or undefined matrixes, seems to work fine

--- cachematrix.R
+++ cachematrix.R
@@ -41,19 +41,45 @@ makeCacheMatrix <- function(x = matrix()) {
 ## invertible using the solve() function in R 
 ## and caches the result for future use
 
+## Use:
+## When a new cached matrix is created with makeCacheMatrix
+## it doesn't automatically calculates the inverse, to calculate
+## and cache the inverse type:
+## Mat <- makeCacheMatrix() ## empty matrix of 1x1
+## cacheSolve(Mat) ## returns Null because matrix is undefined
+## Mat$set(matrix(nrow=2,ncol=2)) ## set the matrix to a 2x2
+## cacheSolve(Mat) ## returns Null because matrix is incomplete
+## mm <- matrix(nrow=2,ncol=2) ## new matrix with no values
+## mm[1,1] <- 1
+## mm[1,2] <- 3
+## mm[2,1] <- -2
+## Mat$set(mm) ## set the matrix to an incomplete 2x2
+## cacheSolve(Mat) ## returns Null because matrix is incomplete
+## mm[2,2] <- 2
+## Mat$set(mm) ## set the matrix to an complete 2x2
+## cacheSolve(Mat) ## returns and caches inverse
 cacheSolve <- function(x, ...) {
-  if (nrow(x$get()) == 1 && is.na(x$get())) {
-    message("undefined matrix returning null")
-    return(NULL)
-  }
-  if (nrow(x$get()[!complete.cases(x$get()),]) > 0) {
-    message("incomplete matrix returning null")
-    return(NULL)
-  }
+  ## If inverse in cache then return it
   invM <- x$getInv()
   if (!is.null(invM)) {
     message("getting cached inverse")
     return(invM)
+  }
+  ## Check if matrix is not undefined
+  if (nrow(x$get()) == 1 && is.na(x$get())) {
+    message("undefined matrix returning null")
+    return(NULL)
+  }
+  ## Check if matrix contains no NA
+  if (is.null(nrow(x$get()[!complete.cases(x$get()),])) ||
+      nrow(x$get()[!complete.cases(x$get()),]) > 0) {
+    message("incomplete matrix returning null")
+    return(NULL)
+  }
+  ## Check if matrix is square
+  if (nrow(x$get()) != ncol(x$get())) {
+    message("matrix not square returning null")
+    return(NULL)
   }
   invM <- solve(x$get())
   x$setInv(invM)
